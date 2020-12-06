7fe3ce556f57e5a87184c4eed0ca61ccc1859bc8
Benjamin-Kavanagh <benjamin.kavanagh@tuni.fi>
2020-12-06 21:14:27 +0200

    Concatenate noisy and standard

--- LSTM_noisydata.py
+++ LSTM_noisydata.py
@@ -6,60 +6,30 @@ Created on Sun Dec  6 17:16:18 2020
 @author: ben
 """
 import numpy as np
-import csv
 import keras
-from librosa.feature import melspectrogram
-from librosa import core
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import KFold
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Conv1D, Flatten, Bidirectional, LSTM, Dropout, BatchNormalization
-from tensorflow.keras.preprocessing import image
 import matplotlib.pyplot as plt
 from skimage.util import random_noise
 
 
 # Load data 
-train_data = np.load('/content/drive/MyDrive/bird_detection/train_data.npy')
-train_labels = np.load('/content/drive/MyDrive/bird_detection/train_labels.npy')
-test_data = np.load('/content/drive/MyDrive/bird_detection/test_data.npy')
+train_data = np.load('train_data.npy')
+train_labels = np.load('train_labels.npy')
+test_data = np.load('test_data.npy')
 train_labels = np.asarray(train_labels, dtype = int)
 
 # Adding Gaussian Noise to data
 noisy_X = np.zeros(train_data.shape)
 for n in range(train_data.shape[0]):
-    noisy_X[n, ...] = random_noise(train_data[n, ...], mode='gaussian', mean=0, var=5)
+    noisy_X[n, ...] = random_noise(train_data[n, ...], mode='gaussian', mean=0, var=0.5)
 
-np.save("noisy_train_data.npy", noisy_X)
+train_data = np.concatenate(train_data, noisy_X)
+train_labels = np.concatenate(train_labels, train_labels)
 
-#%%
-
-
-def data_loader(data_files, label_files, batch_size):
-    while True:
-
-        for n in range(len(data_files)):
-          data = np.load(data_files[n])
-          labels = np.load(label_files[n])
-          labels = np.asarray(labels, dtype = int)
-
-          batch_start = 0
-          batch_end = batch_size
-          L = labels.shape[0]
-          while batch_start < L:
-              limit = min(batch_end, L)
-              X = data[batch_start:limit]
-              Y = labels[batch_start:limit]
-
-              yield (X,Y)
-
-              batch_start += batch_size
-              batch_end += batch_size
-              
-data_files = ["train_data.npy", "noisy_train_data.npy"]
-label_files = ["train_labels.npy", "train_labels.npy"]
-
+train_data, X_valid, train_labels, y_valid = train_test_split(train_data, train_labels, test_size = 0.2)
 
 #%%
 opt = keras.optimizers.Adam(learning_rate=0.002)
@@ -71,11 +41,16 @@ model.add(Dense(1, activation = 'sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer = opt, metrics = ['accuracy'])
 model.summary()
 
+history = model.fit(train_data, train_labels, batch_size=32, epochs = 2, validation_data = (X_valid, y_valid))
 
 #%%
-history = model.fit(data_loader(data_files, label_files, 80), epochs=1, use_multiprocessing=False)
 
-test_data = np.load('/content/drive/MyDrive/bird_detection/test_data.npy')
+del train_data
+del train_labels
+del X_valid
+del y_valid
+
+test_data = np.load('test_data.npy')
 
 probs = model.predict(test_data, verbose=1)
 
